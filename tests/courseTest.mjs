f837a648768cf871121d0599e3d946034dbdac80
Saju <vijayasajanuki@gmail.com>
2023-03-15 18:05:11 -0230

    Node modules updated

---
+++ tests/courseTest.mjs
@@ -0,0 +1,30 @@
+import axios from 'axios';
+var myurl = 'http://127.0.0.1:3000';
+
+// Let's configure the base url
+const instance = axios.create({
+    baseURL: myurl,
+    timeout: 1000,
+    headers: {'content-type': 'application/json'}
+});
+
+async function getAll(){
+    try{
+        let res1 = await instance.get('/fall')
+        console.log('Listing courses')
+        console.log(res1.data)
+    }catch(err){
+        console.log('ERROR: '+err)
+    }
+}
+
+async function getAll(){
+    try{
+        let res1 = await instance.get('/fall')
+        console.log('Listing courses')
+        console.log(res1.data)
+    }catch(err){
+        console.log('ERROR: '+err)
+    }
+}
+getAll();
