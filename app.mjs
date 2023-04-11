db2b604b3b87d80db1d5270b4fd80ebb6d37125a
Saju <vijayasajanuki@gmail.com>
2023-04-11 16:15:37 -0230

    updated server to support client

--- app.mjs
+++ app.mjs
@@ -1,9 +1,8 @@
-import express, { json, urlencoded } from 'express';
+import express from 'express';
+import bodyParser from 'body-parser'
 const app = express();
 const port = 3000;
-
-app.use(json());// support json encoded bodies
-app.use(urlencoded({extended: true}));//incoming objects are strings or arrays
+app.use(bodyParser.json()); // support json encoded bodies
 
 import { add,list_all,find_crn,find_subject,find_subject_number } from './controller/courses.mjs';
 import { find_student_ID,find_student_email,add_reg_course,find_student_info,drop_reg_course } from './controller/students.mjs';
@@ -12,38 +11,47 @@ import { connectToDB, closeDBConnection } from './utils/db.mjs';
 
 var server;
 
-async function createServer() {
-    try {
-        await connectToDB();
-        //course API
-        app.get('/fall',list_all);
-        app.get('/fall/:crn', find_crn);
-        app.get('/fall2/:subject', find_subject);
-        app.get('/fall2/:subject/:number', find_subject_number);
-        //student API
-        app.get('/students/:student_id',find_student_ID);
-        app.get('/students2/:email',find_student_email);
-        app.put('/students3/:student_id', add_reg_course);
-        app.put('/students4/:student_id', drop_reg_course);
-        //professor API
-        app.get('/professors/:employee_id',find_prof_info);
-        // start the server
-        server = app.listen(port, () => {
-          console.log('Example app listening at http://localhost:%d', port);
-        });
-      }catch(err){
-        console.log(err)
-      }
-}
+async function loadDBClient() {
+  await connectToDB();
+}; 
 
-createServer();
+await loadDBClient();
 
-//avoid creating multiple database connections
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(__dirname + '/view'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any domain
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow the HTTP methods you need
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+//resourse paths
+//course API
+app.get('/fall',list_all);
+app.get('/fall/:crn', find_crn);
+app.get('/fall2/:subject', find_subject);
+app.get('/fall2/:subject/:number', find_subject_number);
+//student API
+app.get('/students/:student_id',find_student_ID);
+app.get('/students2/:email',find_student_email);
+app.put('/students3/:student_id', add_reg_course);
+app.put('/students4/:student_id', drop_reg_course);
+//professor API
+app.get('/professors/:employee_id',find_prof_info);
+
+server = app.listen(port, () => {
+  console.log('Example app listening at http://localhost:%d', port);
+});
+
 process.on('SIGINT', () => {
   console.info('SIGINT signal received.');
   console.log('Closing Mongo Client.');
-  server.close(async function(){
-    let msg = await closeDBConnection()   ;
-    console.log(msg);
+  closeDBConnection();
+  server.close(() => {
+    console.log('Http server closed.');
   });
 });
